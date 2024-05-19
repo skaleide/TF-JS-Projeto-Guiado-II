6715ef145cab724cdc62858062dcb4fcf47e2ec3
Skaleide <98890305+skaleide@users.noreply.github.com>
2024-05-19 17:19:26 -0300

    Update livro.ts

--- src/livro.ts
+++ src/livro.ts
@@ -1,13 +1,29 @@
-/*
-// --------- [Não mexer] Responsaveis por renderizar no html
-criarElementoHTML(): HTMLLIElement {
-  const li = document.createElement("li");
-  li.innerHTML = `<span>${this.titulo}</span> (Autor: ${this.autor})`;
-  li.classList.add("livro-item");
-  if (!this.disponivel) {
-    li.classList.add("emprestado");
-  }
-  return li;
+class Livro {
+    constructor( 
+        public id: number, 
+        public titulo: string, 
+        public autor: string, 
+        public estaDisponivel: boolean = true
+    ){}
+
+    emprestarLivro(): void {
+        if(this.estaDisponivel){
+            this.estaDisponivel = false;
+         }else{
+            alert(`O livro "${this.titulo}" não está disponível para emprestimo`);
+
+
+        }
+
+    }
+
+    criarElementoHTML(): HTMLLIElement {
+        const li = document.createElement("li");
+        li.innerHTML = `<span>${this.titulo}</span> (Autor: ${this.autor})`;
+        li.classList.add("livro-item");
+        if (!this.estaDisponivel) {
+          li.classList.add("emprestado");
+        }
+        return li;
+      }
 }
-// --------- [Não mexer] Responsaveis por renderizar no html
-*/
